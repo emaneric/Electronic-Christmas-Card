63458c0a7cec799420ece17f0617b84e025c8f6a
emaneric <eric.n.tech@gmail.com>
2026-09-11 22:51:12 +0100

    Started from scratch with minimal code for bare metal development

--- christmas-card-code/Src/main.c
+++ christmas-card-code/Src/main.c
@@ -1,75 +1,9 @@
 #include "main.h"
-#include "stm32g0xx.h"
-
-void SystemClock_Config(void);
 
 int main(void)
 {
-
-  /** Initialize MCU:
-  1. Configure clocks
-  2. Configure GPIO
-  3. Configure Timers
-  **/
-  
-  SystemClock_Config();
-
- 
   while (1)
   {
-    
+
   }
-  
-}
-
-void SystemClock_Config(void)
-{
-
-  //After reset, voltage scaling is set to Range 2, the max clock using LSI in this mode is 16MHz.
-  //VOS[1:0] bits in Power control register 1 (PWR_CR1) select the range
-
-
-  //Check LSE register
-  uint32_t bdcr = RCC->BDCR;
-  __NOP();
-
-
-
-
-
-
-
-
-  // RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  // RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  // /** Configure the main internal regulator output voltage
-  // */
-  // HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  // /** Initializes the RCC Oscillators according to the specified parameters
-  // * in the RCC_OscInitTypeDef structure.
-  // */
-  // RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  // RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  // RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
-  // RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  // RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  // if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  // {
-  //   Error_Handler();
-  // }
-
-  // /** Initializes the CPU, AHB and APB buses clocks
-  // */
-  // RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-  //                             |RCC_CLOCKTYPE_PCLK1;
-  // RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  // RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  // RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-  // if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  // {
-  //   Error_Handler();
-  // }
 }
